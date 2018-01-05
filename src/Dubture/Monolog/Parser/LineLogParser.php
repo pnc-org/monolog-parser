62619ed027d041f9f9b1308bf03c6e62631d817a
Kristijan Burnik <kristijanburnik@gmail.com>
2018-01-05 14:18:06 +0100

    Fix log parsing.

--- src/Dubture/Monolog/Parser/LineLogParser.php
+++ src/Dubture/Monolog/Parser/LineLogParser.php
@@ -17,39 +17,77 @@ namespace Dubture\Monolog\Parser;
  */
 class LineLogParser implements LogParserInterface
 {
-    protected $pattern = '/\[(?P<date>.*)\] (?P<logger>\w+).(?P<level>\w+): (?P<message>.*[^ ]+) (?P<context>[^ ]+) (?P<extra>[^ ]+)/';
-    
-    /**
-     * Constructor
-     * @param string $pattern
-     */
-    public function __construct($pattern = null)
-    {
-        $this->pattern = ($pattern) ?: $this->pattern;
+  protected $pattern = '/\[(?P<date>.*)\] (?P<logger>\w+).(?P<level>\w+): (?P<rest>.*)/';
+
+  /**
+   * Constructor
+   * @param string $pattern
+   */
+  public function __construct($pattern = null)
+  {
+    $this->pattern = ($pattern) ?: $this->pattern;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function parse($log)
+  {
+    if( !is_string($log) || strlen($log) === 0) {
+      return array();
     }
-    
-    /**
-     * {@inheritdoc}
-     */
-    public function parse($log)
-    {
-        if( !is_string($log) || strlen($log) === 0) {
-            return array();
-        }
 
-        preg_match($this->pattern, $log, $data);
+    preg_match($this->pattern, $log, $data);
 
-        if (!isset($data['date'])) {
-            return array();
-        }
 
-        return array(
-            'date' => \DateTime::createFromFormat('Y-m-d H:i:s', $data['date']),
-            'logger' => $data['logger'],
-            'level' => $data['level'],
-            'message' => $data['message'],
-            'context' => json_decode($data['context'], true),
-            'extra' => json_decode($data['extra'], true)
-        );
+    $this->parseRest($data);
+
+    if (!isset($data['date'])) {
+      return array();
     }
+
+    return array(
+      'date' => \DateTime::createFromFormat('Y-m-d H:i:s', $data['date']),
+      'logger' => $data['logger'],
+      'level' => $data['level'],
+      'message' => $data['message'],
+      'context' => json_decode($data['context'], true),
+      'extra' => json_decode($data['extra'], true)
+    );
+  }
+
+  private function parseRest(&$data) {
+    $jsons = array();
+    $rest = $data['rest'];
+    unset($data['rest']);
+    $parts = explode(" ", $rest);
+    for ($i = 0; $i < count($parts); $i++) {
+      for ($j = $i; $j < count($parts); $j++) {
+        $segment = implode(" ", array_slice($parts, $i, $j - $i + 1));
+        $decoded = json_decode($segment, true);
+        if (json_last_error() == JSON_ERROR_NONE) {
+          $jsons[$i] = $segment;
+          $i = $j;
+          continue;
+        }
+      }
+    }
+
+    krsort($jsons);
+    $messageEndPos = count($jsons);
+    $keys = array("extra", "context");
+    $i = 0;
+    foreach ($jsons as $pos => $value) {
+      if ($i < count($keys)) {
+        $data[$keys[$i]] = $value;
+      }
+      $i++;
+      $messageEndPos = min($pos, $messageEndPos);
+      if ($i >= count($keys)) {
+        break;
+      }
+    }
+
+    $data['message'] = implode(" ", array_slice($parts, 0, $messageEndPos));
+  }
 }
--- tests/Dubture/Monolog/Reader/Test/ParserTest.php
+++ tests/Dubture/Monolog/Reader/Test/ParserTest.php
@@ -17,18 +17,76 @@ use Dubture\Monolog\Parser\LineLogParser;
  * Class ParserTest
  * @package Dubture\Monolog\Reader\Test
  */
-class ParserTest extends \PHPUnit_Framework_TestCase
-{
-    public function testLineFormatter()
-    {
-        $parser = new LineLogParser();
-        $log = $parser->parse('[2013-03-16 14:19:51] test.INFO: foobar {"foo":"bar"} []');
+class ParserTest extends \PHPUnit_Framework_TestCase {
+  public function testLineFormatter() {
+    $parser = new LineLogParser();
+    $log = $parser->parse('[2013-03-16 14:19:51] test.INFO: foobar {"foo":"bar"} []');
 
-        $this->assertInstanceOf('\DateTime', $log['date']);
-        $this->assertEquals('test', $log['logger']);
-        $this->assertEquals('INFO', $log['level']);
-        $this->assertEquals('foobar', $log['message']);
-        $this->assertArrayHasKey('foo', $log['context']);
+    $this->assertInstanceOf('\DateTime', $log['date']);
+    $this->assertEquals('test', $log['logger']);
+    $this->assertEquals('INFO', $log['level']);
+    $this->assertEquals('foobar', $log['message']);
+    $this->assertEquals(array("foo" => "bar"), $log['context']);
+    $this->assertEquals(array(), $log['extra']);
+  }
 
-    }
+  public function testLineFormatter2() {
+    $parser = new LineLogParser();
+    $log = $parser->parse('[2013-03-16 14:19:51] test.INFO: foo bar {"foo":["bar"]} []');
+
+    $this->assertInstanceOf('\DateTime', $log['date']);
+    $this->assertEquals('test', $log['logger']);
+    $this->assertEquals('INFO', $log['level']);
+    $this->assertEquals('foo bar', $log['message']);
+    $this->assertEquals(array("foo" => array("bar")), $log['context']);
+    $this->assertEquals(array(), $log['extra']);
+  }
+
+  public function testLineFormatter3() {
+    $parser = new LineLogParser();
+    $log = $parser->parse('[2013-03-16 14:19:51] test.INFO: foo bar {"foo":["bar"]} {"bar":["foo"]}');
+
+    $this->assertInstanceOf('\DateTime', $log['date']);
+    $this->assertEquals('test', $log['logger']);
+    $this->assertEquals('INFO', $log['level']);
+    $this->assertEquals('foo bar', $log['message']);
+    $this->assertEquals(array("foo" => array("bar")), $log['context']);
+    $this->assertEquals(array("bar" => array("foo")), $log['extra']);
+  }
+
+  public function testLineFormatter4() {
+    $parser = new LineLogParser();
+    $log = $parser->parse('[2013-03-16 14:19:51] test.INFO: foo bar [] {"bar":["foo"]}');
+
+    $this->assertInstanceOf('\DateTime', $log['date']);
+    $this->assertEquals('test', $log['logger']);
+    $this->assertEquals('INFO', $log['level']);
+    $this->assertEquals('foo bar', $log['message']);
+    $this->assertEquals(array(), $log['context']);
+    $this->assertEquals(array("bar" => array("foo")), $log['extra']);
+  }
+
+  public function testLineFormatter5() {
+    $parser = new LineLogParser();
+    $log = $parser->parse('[2013-03-16 14:19:51] test.INFO: foo bar [] {"1":"2"} {"foo":["bar"]} []');
+
+    $this->assertInstanceOf('\DateTime', $log['date']);
+    $this->assertEquals('test', $log['logger']);
+    $this->assertEquals('INFO', $log['level']);
+    $this->assertEquals('foo bar [] {"1":"2"}', $log['message']);
+    $this->assertEquals(array("foo" => array("bar")), $log['context']);
+    $this->assertEquals(array(), $log['extra']);
+  }
+
+  public function testLineFormatter6() {
+    $parser = new LineLogParser();
+    $log = $parser->parse('[2013-03-16 14:19:51] test.INFO: foo bar [] {"1":"2"} {"foo":["bar"]} {"bar":["foo"]}');
+
+    $this->assertInstanceOf('\DateTime', $log['date']);
+    $this->assertEquals('test', $log['logger']);
+    $this->assertEquals('INFO', $log['level']);
+    $this->assertEquals('foo bar [] {"1":"2"}', $log['message']);
+    $this->assertEquals(array("foo" => array("bar")), $log['context']);
+    $this->assertEquals(array("bar" => array("foo")), $log['extra']);
+  }
 }
